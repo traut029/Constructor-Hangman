1b22703d60da822aec5ee3d8e9955daef86319de
Eric Trautmiller <trautmillereahs@gmail.com>
2018-04-04 14:52:21 -0500

    functional

--- index.js
+++ index.js
@@ -3,35 +3,71 @@ var inquirer = require("inquirer")
 
 //bank of words
 var wordBank = ["magnificent", "gallant", "kettle", "edible", "pie", "languini", "computer", "ethereal", "poster", "fantastic"]
-//Picks a random number between 0 and 9, and uses it to pick a word from wordBank
-var randomWord = wordBank[Math.floor(Math.random() * 10)];
-//Store word with Word construct
-var word = new Word(randomWord)
-//Guesses left
-var guesses = 10;
+var startUp = function () {
+    //Picks a random number between 0 and 9, and uses it to pick a word from wordBank
+    var randomWord = wordBank[Math.floor(Math.random() * 10)];
+    //Store word with Word construct
+    var word = new Word(randomWord)
+    //populate character array in word construction
+    word.fill();
+    //Display the initial string
+    word.string()
+    //Guesses left
+    var guesses = 10;
+    //print inital hidden word
+    console.log(word.displayString)
+
+
 //inquirer prompt
 var userGuessLetter = function () {
-    if (guesses > 0) {
+    if (!word.displayString.includes("_")) {
+        console.log("YOU WIN! NEXT WORD!")
+        //initiate restart up function
+        startUp();
+    }
+    else if (guesses > 0) {
         inquirer
             .prompt([
                 {
                     type: "input",
-                    message: "Guess a letter!\n",
+                    message: "Guess a letter!",
 
                     name: "guess",
-
-                    suffix: "_ _ _",
                 }
 
             ]).then(function (answers) {
+                //check if the guessed letter is correct
+                word.wordCheck(answers.guess)
+                //last display to compare to new display
+                var oldDisplay = word.displayString;
+                //recheck the display
+                word.string()
+                //remove one from guesses IF the guess was incorrect
+                if (oldDisplay == word.displayString) {
+                    //subtract from guesses
+                    guesses--;
+                    //console log incorrect
+                    console.log("INCORRECT!")
+                    //console log the number of guesses left
+                    console.log(guesses + " guesses left!")
+                }
 
-                //remove one from guesses
-                guesses--;
+                console.log(word.displayString)
+
                 //run userGuessLetter function again
-                //userGuessLetter();
+                userGuessLetter();
             });
     }
+    else {
+
+        console.log("You lose! The correct answer was " + word.word + "! NEXT WORD!")
+        //initiate retart function
+        startUp();
+    }
+}
+// iniate with userGuessLetter function
+userGuessLetter();
 }
 
 // calls userGuessLetter to run our code
-userGuessLetter();
+startUp();
